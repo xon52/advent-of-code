585fd41a66c7518c7aa59ff0339eb9449fbd0cea
Keagan Chisnall <keagan@chisnall.io>
2023-12-01 23:48:54 +1000

    Update Day 1 2023

---
+++ 2023/Day 1/Part B.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import { URL } from 'url';
+const __dirname = new URL('.', import.meta.url).pathname;
+const dir = decodeURI(__dirname.slice(1));
+
+const getData = (target) => {
+	const text = fs.readFileSync(path.join(dir, `${target}.txt`), 'utf8');
+	const rows = text.split(`\r\n`);
+	return rows;
+};
+
+
+const re = /(one|two|three|four|five|six|seven|eight|nine|[0-9])/g;
+
+const textToDigit = (txt) => {
+	if (txt.match(/^([0-9])$/g)) return +txt;
+	if (txt === 'one') return 1;
+	if (txt === 'two') return 2;
+	if (txt === 'three') return 3;
+	if (txt === 'four') return 4;
+	if (txt === 'five') return 5;
+	if (txt === 'six') return 6;
+	if (txt === 'seven') return 7;
+	if (txt === 'eight') return 8;
+	if (txt === 'nine') return 9;
+	throw new Error(`textToDigit could not interpret ${txt}`);
+};
+
+const getFirstNumber = (str) => {
+	for (let i = 0; i < str.length + 1; i++) {
+		const match = str.substring(0, i).match(re);
+		if (match) return textToDigit(match[0]);
+	}
+};
+
+const getLastNumber = (str) => {
+	for (let i = str.length - 1; i > -1; i--) {
+		const match = str.substring(i).match(re);
+		if (match) return textToDigit(match[0]);
+	}
+};
+
+const run = (puzzle) => {
+	const data = getData(puzzle);
+	const firstAndLast = data.map((e) => `${getFirstNumber(e)}${getLastNumber(e)}`);
+	const result = firstAndLast.reduce((prev, curr) => +prev + +curr, 0);
+	console.log(puzzle, result);
+};
+
+// run('sample2');
+run('puzzle');
