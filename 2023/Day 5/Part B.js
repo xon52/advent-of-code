c6a862ea6b595a802b77e943819067b4fe862007
Keagan Chisnall <keagan@chisnall.io>
2023-12-05 21:28:39 +1000

    Day 5 2

--- 2023/Day 5/Part B.js
+++ 2023/Day 5/Part B.js
@@ -1,53 +1,64 @@
 import { getFileData } from '../../helpers/index.js';
 
-const dataSplitter = (str) =>
-	str
-		.split(':|')[1]
-		.split('|')
-		.map((e) => e.split(' ').map((f) => +f));
+const data = getFileData(import.meta, 'puzzle');
+const data2 = data.join('|').split('||');
 
-const convert = (seed, ranges) => {
-	// Default is 1:1
-	let result = seed;
-	// If found in special ranges set result
-	for (let r = 0; r < ranges.length; r++) {
-		const [dest, source, range] = ranges[r];
-		if (seed >= source && seed <= source + range) return dest + seed - source;
-	}
-	return result;
-};
+// Define seeds
+const seeds = data2[0]
+	.split(': ')[1]
+	.split(' ')
+	.map((e) => +e);
 
-const run = (puzzle) => {
-	const data = getFileData(import.meta, puzzle);
-	const data2 = data.join('|').split('||');
-	// Define seeds
-	const seeds = data2[0]
-		.split(': ')[1]
-		.split(' ')
-		.map((e) => +e);
+// Define maps
+const maps = [];
+for (let i = 1; i < data2.length; i++)
+	maps.push(
+		data2[i]
+			.split(':|')[1]
+			.split('|')
+			.map((e) => e.split(' ').map((f) => +f))
+	);
 
-	// Define stages
-	const stages = [];
-	for (let i = 1; i < data2.length; i++) stages.push(dataSplitter(data2[i]));
-
-	// Work through seeds
-	let min = Infinity;
-	const seeds2 = [];
-	for (let s = 0; s <= seeds.length / 2; s += 2) {
-		for (let i = seeds[s]; i <= seeds[s] + seeds[s + 1]; i++) {
-			let _result = i;
-			for (let s = 0; s < stages.length; s++) {
-				_result = convert(_result, stages[s]);
-			}
-			if (_result < min) {
-				min = _result;
-				console.log('New min', min);
+// Get location from seed
+const getLocation = (seed) => {
+	for (let m = 0; m < maps.length; m++) {
+		const map = maps[m];
+		for (let r = 0; r < map.length; r++) {
+			const [dest, source, range] = map[r];
+			if (seed >= source && seed < source + range) {
+				seed = dest + seed - source;
+				break;
 			}
 		}
 	}
-
-	console.log(puzzle, min);
+	return seed;
 };
 
-run('sample');
-run('puzzle');
+console.log(getLocation(2250051846));
+
+// Get approx
+let min = Infinity;
+let minSeed = undefined;
+let delta = 10000;
+for (let s = 0; s <= seeds.length / 2; s += 2) {
+	const start = seeds[s];
+	const end = seeds[s] + seeds[s + 1];
+	console.log('Pair', s / 2 + 1, seeds[s], seeds[s + 1]);
+	for (let seed = start; seed <= end; seed += delta) {
+		if (getLocation(seed) < min) {
+			min = getLocation(seed);
+			minSeed = seed;
+			console.log('New min', min, seed);
+		}
+	}
+}
+
+// Get exact
+const start = minSeed - delta;
+const end = minSeed + delta;
+for (let seed = start; seed <= end; seed++) {
+	if (getLocation(seed) < min) {
+		min = getLocation(seed);
+		console.log('New min', min);
+	}
+}
