f1b072aa95f0a4417d7074525772cab1a7198bb0
Keagan Chisnall <keagan@chisnall.io>
2023-12-29 15:16:04 +1000

    Day 20

--- 2023/Day 20/index.js
+++ 2023/Day 20/index.js
@@ -1,41 +1,91 @@
+import { count } from 'console';
 import { getFileData } from '../../helpers/index.js';
 import { create2DArray } from '../../helpers/utils.js';
 import { EOL } from 'os';
 
 // Setup
-const target = 'sample';
+const target = 'sample2';
 const data = getFileData(import.meta, target);
 const modules = {};
+let highPulseCount = 0;
+let lowPulseCount = 0;
 
-data.map(e=>{
-  const [_id, _instructions] = e.split(' -> ');
-  let type = 'unknown'
-  if(id.match(/%[a-z]+/)) type = 'flip-flop'
-  else if(id.match(/&[a-z]+/)) type = 'converter'
-  else if (id==='broadcast') type = 'broadcast'
-  const instructions = _instructions.split(', ');
-  modules[id] = { type, instructions };
-})
+data.map((e) => {
+	let [id, instructions] = e.split(' -> ');
+	let type = 'unknown';
+	if (id.match(/%[a-z]+/)) {
+		id = id.replace('%', '');
+		type = 'flip-flop';
+	} else if (id.match(/&[a-z]+/)) {
+		id = id.replace('&', '');
+		type = 'conjunction';
+	} else if (id === 'broadcaster') type = 'broadcaster';
+	instructions = instructions.split(', ');
+	modules[id] = { id, type, instructions, state: 'off', memory: {} };
+});
 
-const process = (id, strength) => {
-  const module = modules[id];
-  }
-}
+// Populate conjunction memory
+Object.values(modules)
+	.filter((m) => m.type === 'conjunction')
+	.forEach((conjunction) => {
+		Object.keys(modules).forEach((id) => {
+			const module = modules[id];
+			if (module.instructions.includes(conjunction.id)) conjunction.memory[id] = 'low';
+		});
+	});
 
-const broadcast = (strength) => {
-  modules['broadcast'].instructions.forEach()
-}
-
+const processQueue = [];
+const process = (from, to, strength) => {
+	// Count pulses
+	strength === 'low' ? lowPulseCount++ : highPulseCount++;
+	// Process module
+	const module = modules[to];
+	if (!module) return;
+	switch (module.type) {
+		case 'flip-flop':
+			if (strength === 'low') {
+				strength = module.state === 'off' ? 'high' : 'low';
+				module.state = module.state === 'off' ? 'on' : 'off';
+			} else return;
+			break;
+		case 'conjunction':
+			if (module.memory[from]) module.memory[from] = modules[from].state;
+			const active = Object.values(module.memory).every((v) => v === 'on');
+			if (active) strength = 'low';
+			break;
+		case 'broadcaster':
+			break;
+		default:
+			break;
+	}
+	// Send pulse to next module
+	module.instructions.forEach((nextModule) => {
+		// console.log(id, strength, '->', nextModule);
+		processQueue.push({
+			from: module.id,
+			to: nextModule,
+			strength,
+		});
+	});
+};
 
 // ===========================================================
 // PART 1
 // ===========================================================
-// for (let p = 0; p < parts.length; p++) {
-// 	const part = parts[p];
-// 	runWorkflow(workflows[startingWorkflow], part);
-// }
-// let answer1 = accepted.reduce((p, c) => p + c.x + c.m + c.a + c.s, 0);
-// console.log(answer1);
+for (let i = 0; i < 1000; i++) {
+	console.log('==============================');
+	console.log('Button press', i + 1);
+	console.log('==============================');
+	process('', 'broadcaster', 'low');
+	while (processQueue.length) {
+		const { from, to, strength } = processQueue.shift();
+		console.log(from, strength, '->', to);
+		process(from, to, strength);
+	}
+}
+console.log('lowPulseCount', lowPulseCount, 'highPulseCount', highPulseCount);
+let answer1 = lowPulseCount * highPulseCount;
+console.log(answer1);
 
 // ===========================================================
 // PART 2
