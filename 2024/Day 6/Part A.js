75c6d9540e99aac39fbed985a1351c2710f16bb8
Keagan Chisnall <keagan@chisnall.io>
2024-12-05 17:05:09 +1000

    Checkpoint

---
+++ 2024/Day 6/Part A.js
@@ -0,0 +1,10 @@
+import { getFileData } from '../../helpers/index.js';
+
+const run = (puzzle) => {
+	const data = getFileData(import.meta, puzzle);
+
+	// console.log(puzzle, result);
+};
+
+run('sample');
+// run('puzzle');
