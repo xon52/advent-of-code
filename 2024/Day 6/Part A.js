8d858f27775c71f5feb666c4f7ce36fabf4bccf2
Keagan Chisnall <keagan@chisnall.io>
2024-12-07 11:21:05 +1000

    Refactor Day 6 solutions: enhance grid processing and add example data

--- 2024/Day 6/Part A.js
+++ 2024/Day 6/Part A.js
@@ -1,10 +1,94 @@
 import { getFileData } from '../../helpers/index.js';
 
+/*
+Example data:
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+
+^ - Guard starting point and direction (up)
+# - Wall
+
+Example result:
+....#.....
+....XXXXX#
+....X...X.
+..#.X...X.
+..XXXXX#X.
+..X.X.X.X.
+.#XXXXXXX.
+.XXXXXXX#.
+#XXXXXXX..
+......#X..
+
+X - Path
+
+Final answer is the number of Xs in the result (41).
+
+*/
+
 const run = (puzzle) => {
 	const data = getFileData(import.meta, puzzle);
+	const grid = data.map((row) => row.split(''));
+	const [startX, startY] = grid.reduce((acc, row, y) => {
+		const x = row.indexOf('^');
+		if (x > -1) {
+			acc = [x, y];
+		}
+		return acc;
+	}, []);
 
-	// console.log(puzzle, result);
+	const directions = [
+		[0, -1], // Up
+		[1, 0], // Right
+		[0, 1], // Down
+		[-1, 0], // Left
+	];
+
+	let x = startX;
+	let y = startY;
+	let direction = 0;
+
+	const stepForward = () => {
+		try {
+			if (grid[y][x] === '.' || grid[y][x] === '^') {
+				grid[y][x] = 'X';
+			}
+			let [dx, dy] = directions[direction];
+
+			if (grid[y + dy][x + dx] === '#') {
+				turnRight();
+				[dx, dy] = directions[direction];
+			}
+			x += dx;
+			y += dy;
+			return x >= 0 && x < grid[0].length && y >= 0 && y < grid.length;
+		} catch (e) {
+			return false;
+		}
+	};
+
+	const turnRight = () => {
+		direction = (direction + 1) % 4;
+	};
+
+	while (stepForward()) {
+		// console.log(grid.map((row) => row.join('')).join('\n'));
+	}
+
+	const result = grid.reduce((acc, row) => acc + row.filter((cell) => cell === 'X').length, 0);
+
+	console.log(grid.map((row) => row.join('')).join('\n'));
+
+	console.log(puzzle, result);
 };
 
 run('sample');
-// run('puzzle');
+run('puzzle');
--- 2024/Day 6/Part B.js
+++ 2024/Day 6/Part B.js
@@ -1,58 +1,91 @@
 import { getFileData } from '../../helpers/index.js';
 
+const directions = [
+	[0, -1], // Up
+	[1, 0], // Right
+	[0, 1], // Down
+	[-1, 0], // Left
+];
+
+const findStartingPosition = (grid) =>
+	grid.reduce((acc, row, y) => {
+		const x = row.indexOf('^');
+		if (x > -1) {
+			acc = { x, y };
+		}
+		return acc;
+	}, []);
+
+const getNextPosition = ({ x, y }, direction) => {
+	if (direction === 0) return { x, y: y - 1 };
+	if (direction === 1) return { x: x + 1, y };
+	if (direction === 2) return { x, y: y + 1 };
+	if (direction === 3) return { x: x - 1, y };
+};
+
+function getValueAtPosition(grid, { x, y }) {
+	if (y < 0 || y >= grid.length || x < 0 || x >= grid[0].length) {
+		return undefined;
+	} else {
+		return grid[y][x];
+	}
+}
+
+const rotate90Right = (direction) => {
+	return (direction + 1) % 4;
+};
+
+const placeObstruction = (grid, { x, y }) => {
+	const gridCopy = JSON.parse(JSON.stringify(grid));
+	gridCopy[y][x] = 'O';
+	return gridCopy;
+};
+
+const isLoop = (grid) => {
+	let position = findStartingPosition(grid);
+	let prevPosition = { x: position.x, y: position.y };
+	let direction = 0;
+	let value = '^';
+
+	const visitedPositions = new Set();
+
+	while (value !== undefined) {
+		const positionAndDirection = `${position.y},${position.x},${direction}`;
+
+		if (visitedPositions.has(positionAndDirection)) {
+			return true;
+		}
+
+		visitedPositions.add(positionAndDirection);
+
+		prevPosition = { x: position.x, y: position.y };
+		position = getNextPosition(position, direction);
+		value = getValueAtPosition(grid, position);
+
+		if (value === '#' || value === 'O') {
+			direction = rotate90Right(direction);
+			position.x = prevPosition.x;
+			position.y = prevPosition.y;
+		}
+	}
+
+	return false;
+};
+
 const run = (puzzle) => {
 	const data = getFileData(import.meta, puzzle);
-	const emptyString = data.findIndex((item) => item === '');
-	const rules = data.slice(0, emptyString);
-	const messages = data.slice(emptyString + 1);
-
-	const numbers = {};
-
-	rules.forEach((rule) => {
-		const [before, after] = rule.split('|');
-		if (numbers[before] === undefined) numbers[before] = { before: [], after: [] };
-		if (numbers[after] === undefined) numbers[after] = { before: [], after: [] };
-		numbers[before].after.push(after);
-		numbers[after].before.push(before);
-	});
-
-	const correctMessage = (message) => {
-		const messageArr = message.split(',');
-		const correctArr = [];
-		let skip = false;
-		for (let i = 0; i < messageArr.length; i++) {
-			if (skip) {
-				skip = false;
-				continue;
-			}
-			if (i === messageArr.length - 1) {
-				correctArr.push(messageArr[i]);
-				break;
-			}
-			const current = messageArr[i];
-			const next = messageArr[i + 1];
-			if (numbers[current].after.indexOf(next) === -1) {
-				skip = true;
-				correctArr.push(next);
-				correctArr.push(current);
-			} else {
-				correctArr.push(current);
-			}
-		}
-		return correctArr.join(',');
-	};
+	const grid = data.map((row) => row.split(''));
 
 	let result = 0;
-	messages.forEach((message) => {
-		let correctedMessage = correctMessage(message);
-		if (correctedMessage !== message) {
-			while (correctedMessage !== correctMessage(correctedMessage)) {
-				correctedMessage = correctMessage(correctedMessage);
+
+	for (let y = 0; y < grid.length; y++) {
+		for (let x = 0; x < grid[0].length; x++) {
+			if (grid[y][x] === '.' && isLoop(placeObstruction(grid, { x, y }))) {
+				result++;
 			}
-			// console.log(message, correctedMessage);
-			result += parseInt(correctedMessage.split(',')[Math.floor(correctedMessage.split(',').length / 2)]);
+			console.log(Math.floor((y / grid.length) * 100), '%');
 		}
-	});
+	}
 
 	console.log(puzzle, result);
 };
